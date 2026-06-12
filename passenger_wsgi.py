b3457975318ae542d0fb4aef46a28ff0ffcbc6ed
G.Srikar <guddantisrikar2005@gmail.com>
2026-06-12 16:32:23 +0530

    update passenger_wsgi.py for HostyCare

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-project_home = os.path.dirname(__file__)
+project_home = '/home/gsrikari/SE/receipt_generator'
 
 if project_home not in sys.path:
     sys.path.insert(0, project_home)
